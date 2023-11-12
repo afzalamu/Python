97ef24cd92f01860cb2db98ba4f7402d51236d18
Afzal Malik <124300839+afzalamu@users.noreply.github.com>
2023-11-12 13:26:37 +0530

    Update tuples.py

--- lesson6_Lists&Tupples/tuples.py
+++ lesson6_Lists&Tupples/tuples.py
@@ -6,31 +6,31 @@ print(yourtuple)
 print(type(yourtuple))
 print(type(mytuple))
 
-# # ------------------------------------
+# ------------------------------------
 
-# # Tuple cannot be changed
-# # but we can copy it to do something creative
+# Tuple cannot be changed
+# but we can copy it to do something creative
 
-# newlist = list(mytuple)  # changed it to a list
+newlist = list(mytuple)  # changed it to a list
 
-# print(newlist)
-# print(type(newlist))
-# # now value can be addded or removed
-# newlist.append("Neil")
-# print(newlist)
+print(newlist)
+print(type(newlist))
+# now value can be addded or removed
+newlist.append("Neil")
+print(newlist)
 
-# newtuple = tuple(newlist)
-# # casted a list into tuple
-# print(newtuple)
-# print(type(newtuple))
+newtuple = tuple(newlist)
+# casted a list into tuple
+print(newtuple)
+print(type(newtuple))
 
 
-# ---------------------------------------
-# unpacking a tuple
-# (one, two, *hey) = yourtuple
-# print(one)
-# print(two)
-# print(*hey)
+---------------------------------------
+unpacking a tuple
+(one, two, *hey) = yourtuple
+print(one)
+print(two)
+print(*hey)
 
 (one, *two, hey) = yourtuple
 print(one)
